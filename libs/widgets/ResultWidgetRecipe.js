bb665566a4bfbe7d759d6bb4db6a628e1a393064
Hans-Martin Schuller <hm.schuller@googlemail.com>
2014-10-24 01:17:22 +0200

    pushpush

--- libs/widgets/ResultWidgetRecipe.js
+++ libs/widgets/ResultWidgetRecipe.js
@@ -15,21 +15,16 @@ AjaxSolr.ResultWidgetRecipe = AjaxSolr.AbstractWidget.extend({
     document.getElementById("recipe-title").innerHTML = doc.title;
     document.getElementById("recipe-description").innerHTML = doc.instructions; 
 
-    string1 = doc.ingredient_name;
-    string2 = doc.portion_type;
-    string3 = doc.portion_value;
-    console.log(string1 + string2 + string3 );
+  
 
     ingredientArray = doc.ingredient_name.split(";");
     portionTypeArray = doc.portion_type.split(";");
     portionValueArray = doc.portion_value.split(";");
 
-    console.log(ingredientArray.length );
 
     var output = "<h4 class=\"left-text\">Zutaten:</h4>" + "<ul>";
-    for (var i = 0; i < ingredientArray.length -1; i ++) {
+    for (var i = 0; i < ingredientArray.length - 1; i ++) {
       output += "<li>" + portionValueArray[i] + portionTypeArray[i] + " " + ingredientArray[i] + "</li>"
-      console.log("hi");
     };
     output +=   "</ul>"
    
