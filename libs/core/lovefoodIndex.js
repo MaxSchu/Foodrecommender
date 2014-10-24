bed83f12bde8e343d0c4fd345948ec8d8be82458
Hans-Martin Schuller <hm.schuller@googlemail.com>
2014-10-24 02:35:16 +0200

    added widgets

--- libs/core/lovefoodIndex.js
+++ libs/core/lovefoodIndex.js
@@ -6,17 +6,165 @@ var Manager;
     Manager = new AjaxSolr.Manager({
       solrUrl: 'http://localhost:8983/solr/foodRecommender/'
     });
+    
     Manager.addWidget(new AjaxSolr.ResultWidget({
-      id: 'fastfood',
-      target: '#result'
+      target: '#forMe'
     }));
     Manager.init();
-    queryString = "req_skill:[0 TO 50] AND vegan:true";
+    queryString = getProfileTagString();
+    console.log(queryString);
     Manager.store.addByValue('q', queryString);
     Manager.store.addByValue('rows', '3');
     Manager.doRequest();
-  
-  });
+ 
+   });
   
 
 })(jQuery);
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#diff_easy'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " req_skill:0";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#diff_med'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " req_skill:25";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });    
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#diff_hard'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " req_skill:[50 TO 75]";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#time_easy'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " time_cook:0";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#time_med'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " time_cook:25";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+
+var Manager;
+(function ($) {
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://localhost:8983/solr/foodRecommender/'
+    });
+    Manager.addWidget(new AjaxSolr.ResultWidget({
+      target: '#time_hard'
+    }));
+    Manager.init();
+    queryString = getProfileTagString() + " time_cook:[50 TO 75]";
+    Manager.store.addByValue('q', queryString);
+    Manager.store.addByValue('rows', '3');
+    Manager.doRequest();
+       });
+})(jQuery);
+
+
+
+
+
+
+function getProfileTagString() {
+  var query = "";
+
+  if(localStorage.getItem("taglist") != null) {
+    var tagArray = JSON.parse(localStorage.getItem("taglist"));
+    var colorArray = JSON.parse(localStorage.getItem("colorlist"));
+
+
+    for(var i = 0; i < tagArray.length; i++) {
+      if(colorArray[i] == "red"){
+        query += "-"+tagArray[i];
+      }
+      if(colorArray[i] == "green"){
+        query += "+"+tagArray[i];
+      }
+    }
+  }
+ 
+
+   if(query.indexOf("+") == 0){
+  query = query.substring(1);
+  }  
+ingredientString = query;
+ingredientString = ingredientString.replace(/\+/g, " AND " );
+ingredientString = ingredientString.replace(/\-/g, " NOT " );
+ingredientString = ingredientString.replace(/%C3/g, "" );
+ingredientString = ingredientString.replace(/%84/g, "Ä" );
+ingredientString = ingredientString.replace(/%9C/g, "Ü" );
+ingredientString = ingredientString.replace(/%96/g, "Ö" );
+ingredientString = ingredientString.replace(/%A4/g, "ä" );
+ingredientString = ingredientString.replace(/%BC/g, "ü" );
+ingredientString = ingredientString.replace(/%B6/g, "ö" );
+
+  return ingredientString;
+}
